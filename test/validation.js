abe4501f6a55013987b31eda93aedcc2853f1621
Adriaan Mulder <adriaan.mulder@gmail.com>
2016-01-18 14:25:34 -0800

    cleanup

--- test/validation.js
+++ test/validation.js
@@ -1,68 +1,7 @@
 var assert = require('chai').assert;
 var validation = require('../lib/validation');
-
-var model = {
-  schema : {
-    _increments: true,
-    _timestamps: true,
-    name: {
-      type: 'string',
-      required: true,
-      validate: {
-        min: 3,
-        max: 20
-      }
-    },
-    age: {
-      type: 'integer',
-      required: true,
-      validate: {
-        min: 21
-      }
-    },
-    email: {
-      type: 'string',
-      required: true,
-      validate: {
-        isEmail: true,
-        isEmailInUse: function(value, next) {
-          return next(null, true);
-        }
-      },
-      beforeInsert: function(value, done) {
-        done(null, value.toLowerCase().trim());
-      }
-    },
-    birthday: {
-      type: 'dateTime',
-      required: false
-    },
-    buttchicken: {
-      type: 'boolean',
-      required: true
-    }
-  }
-}
-
-var data = {
-  validWithRequiredPropertiesMissing: {
-    name: "batman",
-    age: 21,
-    email: "batman@example.com",
-    birthday: new Date()
-  },
-  validComplete: {
-    name: "batman",
-    age: 21,
-    email: "batman@example.com",
-    birthday: new Date(),
-    buttchicken: true
-  },
-  invalidType: {
-    name: 10,
-    buttchicken: true
-  }
-}
+var model = getModel();
+var data = getData();
 
 describe('validation', function() {
   it('should attach to model', function(done){
@@ -103,7 +42,7 @@ describe('validation', function() {
       })
     });
 
-    it('should say data is wrong type', function(done) {
+    it('should say when value is the wrong type', function(done) {
       model.validate(data.invalidType, function(err) {
         assert(err.length > 0, 'error was found');
         done()
@@ -111,5 +50,71 @@ describe('validation', function() {
     })
   });
 
-
 });
+
+function getModel() {
+  return {
+    schema : {
+      _increments: true,
+      _timestamps: true,
+      name: {
+        type: 'string',
+        required: true,
+        validate: {
+          min: 3,
+          max: 20
+        }
+      },
+      age: {
+        type: 'integer',
+        required: true,
+        validate: {
+          min: 21
+        }
+      },
+      email: {
+        type: 'string',
+        required: true,
+        validate: {
+          isEmail: true,
+          isEmailInUse: function(value, next) {
+            return next(null, true);
+          }
+        },
+        beforeInsert: function(value, done) {
+          done(null, value.toLowerCase().trim());
+        }
+      },
+      birthday: {
+        type: 'dateTime',
+        required: false
+      },
+      buttchicken: {
+        type: 'boolean',
+        required: true
+      }
+    }
+  }
+}
+
+function getData() {
+  return {
+    validWithRequiredPropertiesMissing: {
+      name: "batman",
+      age: 21,
+      email: "batman@example.com",
+      birthday: new Date()
+    },
+    validComplete: {
+      name: "batman",
+      age: 21,
+      email: "batman@example.com",
+      birthday: new Date(),
+      buttchicken: true
+    },
+    invalidType: {
+      name: 10,
+      buttchicken: true
+    }
+  }
+}
